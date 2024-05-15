e7819c49d6e9502767ad6ab073974298dec4f13a
JeremieAgee <you@example.com>
2024-05-14 19:50:56 -0500

    added script.js and added a list of functions and buttons

---
+++ public/script.js
@@ -0,0 +1,71 @@
+
+let main = document.getElement('main');
+const indexBtn = document.querySelector('#homePgBtn');
+const deckBuilderPgBtn = document.querySelector('#deckBuilderPgBtn');
+const deckSearchPgBtn = document.querySelector('#deckSearchPgBtn');
+const collectionPgBtn = document.querySelector('#collectionPgBtn');
+const loginPgBtn = document.querySelector('#loginPgBtn');
+const signupPgBtn = document.querySelector('#signupPgBtn');
+const deckTesterPgBtn = document.querySelector('#deckTesterPgBtn');
+
+function setHome(){
+    
+}
+async function setDeckBuilderPage(){
+
+}
+async function setDeckSearchPage(){
+
+}
+async function setCollectionPage(){
+
+}
+function setLoginPage(){
+
+}
+function setSignupPage(){
+
+}
+function setDeckTesterPage(){
+
+}
+function addToCollection(card){
+
+}
+function removeFromCollection(card){
+
+}
+async function filterDecks(){
+
+}
+async function filterCards(){
+
+}
+async function addToDeck(card){
+
+}
+async function removeFromDeck(card){
+
+}
+function setViewDeckPage(){
+
+}
+function functionShuffleDeck(deck){
+
+}
+async function signup(){
+
+}
+async function login(){
+
+}
+async function searchCard(){
+    
+}
+indexBtn.onclick = setHome();
+deckBuilderPgBtn.onclick = setDeckBuilderPage();
+deckSearchPgBtn.onclick = setDeckSearchPage();
+collectionPgBtn.onclick = setCollectionPage();
+loginPgBtn.onclick = setLoginPage();
+signupPgBtn.onclick = setSignupPage();
+deckTesterPgBtn.onclick = setDeckTesterPage();
